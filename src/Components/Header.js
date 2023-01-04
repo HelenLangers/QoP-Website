03ca508a7e7bfd7b6eddcd5393146e62305f458d
Helen Langridge <langridge.helen@outlook.com>
2023-01-04 15:15:19 +0000

    add instagram link and icon

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from '../Images/Logo.svg'
+import {FaInstagram} from 'react-icons/fa'
 
 const Header = () => {
   return (
@@ -16,8 +17,8 @@ const Header = () => {
               <li><a href="#whatwedo" className='navlistitems'>What We Do</a></li>
               <li><a href="#principles" className='navlistitems'>Ethos</a></li>
               <li><a href="/" className='navlistitems'>FAQs</a></li>
-              <li><a href="/" className='navlistitems'>Contact</a></li>
               <li><a href="/" className='navlistitems'>Donate</a></li>
+              <li><a href="https://www.instagram.com/queens.of.pain.cc/" className='navlistitems'><FaInstagram size={25} className='icon'/></a></li>
           </ul>
         </nav>
     </header>
--- src/Components/LandingImage.js
+++ src/Components/LandingImage.js
@@ -3,11 +3,11 @@ import React from 'react'
 const LandingImage = () => {
   return (
     <section className='landing-image' id='home'>
-        <div className='tagline-div'>
+        {/* <div className='tagline-div'>
             <h2 className='tagline'>A women-led cycle group based in Edinburgh. 
             <br/>Wednesdays at 7pm, ride for 1h or so, go home happy. 
             <br/>Repeat next week!</h2>
-        </div>
+        </div> */}
     </section>
   )
 }
