656e074aef8e80cc11af0660386da982d2b60dc8
Helen Langridge <langridge.helen@outlook.com>
2023-01-07 17:00:14 +0000

    add new pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Homepage from './Pages/Homepage';
+import FAQs from './Pages/FAQs'
+import Events from './Pages/Events'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Homepage/>}/>
+        <Route path="/faqs" element={<FAQs/>}/>
+        <Route path="/events" element={<Events/>}/>
       </Routes>
     </Router>
   );
---
+++ src/Pages/FAQs.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from '../Components/NavBar/NavBar'
+
+const FAQs = () => {
+  return (
+    <div className='app'>
+      <NavBar/>
+
+      <section className='faq-container'>
+        FAQs here!
+      </section>
+
+
+    </div>
+  )
+}
+
+export default FAQs
